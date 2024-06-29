81c18681749d8a651922472aa3af100d0d11c65d
Sthery <sthery.styll@gmail.com>
2024-06-29 02:23:41 +0100

    update server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,20 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`🔥 Server is running on: http://localhost:${PORT}`)
-})
+if (process.env.DB_SCHEMA) {
+    app.listen(PORT, (err) => {
+        if (err) {
+            console.log(`ERRO ao iniciar o servidor: ${err}`);
+        } else {
+            console.log('Servidor de produção no ar');
+        }
+    });
+} else {
+    app.listen(PORT, (err) => {
+        if (err) {
+            console.log(`Erro ao iniciar o servidor ${err}`);
+        } else {
+            console.log(`🔥 Server is running on: http://localhost:${PORT}`) //servidor teste no ar
+        }
+    })
+}
